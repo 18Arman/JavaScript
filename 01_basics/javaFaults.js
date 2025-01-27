d0271340a4a95a4affb13feba9b11f0be5d1efe6
Arman Soni <114308279+18Arman@users.noreply.github.com>
2025-01-27 17:23:12 +0000

    datatype conversion case study started

---
+++ 01_basics/javaFaults.js
@@ -0,0 +1,28 @@
+let num=123
+console.log(typeof num)
+console.log(num)
+num="abc22"
+console.log(typeof num)
+console.log(num)
+//converting to number
+num=Number(num)//here we will se that type of num is now number but it's value is NaN means not a number
+console.log(typeof num)
+console.log(num)
+num=Number(null)
+console.log(typeof num)
+console.log(num)
+num=Number(undefined)
+console.log(typeof num)
+console.log(num)
+
+//case student for datatype to number
+// so string and undefined are giving Nan and type as number
+// while null is giving 0 and type as number
+
+
+
+// for boolean comparison
+// 0 will give false ,1 will give true,"" empty string is false,"arman" is true
+
+//string conversion
+//if we convert a number to string then it will convert successfully
