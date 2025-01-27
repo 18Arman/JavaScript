0b28f03eec6b7868888398737fb1855400a6fafe
Arman Soni <114308279+18Arman@users.noreply.github.com>
2025-01-27 16:41:41 +0000

    3 Lectures Completed

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -1,1 +1,9 @@
 console.log("Hello,Arman")
+const CustomerID="12206043"
+var CustomerName="Arman"
+let CustomerYear="3rd"
+let Age=20
+City='Kapurthala'
+let mail
+console.log("Printing Customer Details")
+console.table([CustomerID,CustomerName,CustomerYear,Age,City,mail])
