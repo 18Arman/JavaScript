5a3d4aa7f5234884bbcaa16aac644fcfe2e6569e
Arman Soni <114308279+18Arman@users.noreply.github.com>
2025-01-27 17:02:17 +0000

    datatypes and standards

---
+++ 01_basics/datatypes_ECMA.js
@@ -0,0 +1,27 @@
+"use strict";// to treat all code as newer version\
+
+// alert(3+3) //it will not work because we are using nodejs, not browser
+// make code readable
+console.log("Printing" ,3+3)
+
+//String
+let name="Arman"
+//Integer
+let age=21
+//boolean
+let isadult=true
+let state=null
+
+//Primitive datatypes
+//number: 2^53
+//bigint
+//string=>""
+//boolean=>true/false
+//null : standalone value//means value is empty,
+//undefined=> when value is not assigned and variable is declare
+//symbol=>to find uniqueness of a component,
+
+console.log(typeof "Arman")
+console.log(typeof 21)
+console.log(typeof undefined)//it's type is undefined
+console.log(typeof null)//it's type is object
