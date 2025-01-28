edd1e00e44e9d60a40aa138fa4b691e381c70b94
Arman Soni <114308279+18Arman@users.noreply.github.com>
2025-01-28 19:10:31 +0000

    lecture 7 and 9 code completed

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,51 @@
+// Operation
+let num=3
+let neg_num=-num
+// console.log(num)
+// console.log(neg_num)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**4)
+// console.log(2/2)
+// console.log(2%3)
+
+//tricky chize
+// let s1="hello"
+// let s2="dost"
+// let s3=s1+" "+s2
+// console.log(s3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)//se if first is string then it is treating whole as string
+// console.log(1+2+"2")//see if first isn num then it is treating as num till string does'nt appear
+
+//trick2
+// console.log(true)
+// console.log(+true)
+// console.log(-true)
+// console.log(+"")
+
+
+// let num1,num2
+// num1=num2=2+2
+    
+// let gamecounter=1;
+// gamecounter++;
+// console.log(gamecounter);
+
+//lec 8 Comparison
+//problem comes when data type is different in comparison
+//avoid these types of comparisons
+console.log("1"<2);
+console.log("01"<2);
+console.log(null>0);
+console.log(null==0);//the reason is that an equality check == and  comparisons >= <= > <works differently
+console.log(null>=0);//comparisons converts null to 0 that's why here answer is true
+
+//undefined will always give false in any comparison
+console.log(undefined>0);
+
+//=== strict check means it also checks the datatype and does'nt changes the datatype
+
+
