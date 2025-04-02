7ad49ab5242d3f21e7477c8462c1f399fd6d15c4
Arman Soni <114308279+18Arman@users.noreply.github.com>
2025-04-02 11:21:06 +0000

    Objects class 1 and 2 done

---
+++ 01_basics/objects.js
@@ -0,0 +1,72 @@
+//two type of objects, Singleton, literal
+// Singleton: these are those which are created using constructors
+// eg: Object.create
+// literal: these are using the normal method 
+// const user={};
+
+const mysym=Symbol("yoursymbol");
+const user={
+    name:"Arman",
+    Age:"20",
+    mail:"1807arman@gmail.com",
+    //using mysym outside symbol here as symbol
+    [mysym]:"Symbol"
+}
+console.log(user.name);
+console.log(user["name"]);
+console.log(user[mysym]);
+user.greetings=function(){
+    console.log("Namaste!");
+}
+user.greetings2=function(){
+    console.log(`Namaste Again!,${this.name}`)
+}
+
+//we can freeze or lock the change of key values of object
+Object.freeze(user);
+user.name="Jatin";
+console.log(user.name);
+
+//calling the functions
+user.greetings();
+user.greetings2();
+
+
+//Object class 2
+
+//nested object
+const new_user={
+    name:{
+        fullname:{
+            username:{
+                firstname:"arman",
+                lastname:"soni"
+            }
+        }
+    }
+}
+console.log(new_user.name.fullname.username);
+//combining two objects
+//method 1
+let ob3=Object.assign({},new_user,user);
+console.log(ob3);
+//method2
+ob3={...user,...new_user}
+console.log(ob3);
+
+//Objects inside array
+const arr=[
+    user,new_user
+]
+console.log((arr[0].Age));
+
+//print only keys/values
+console.log(Object.keys(user))
+
+//entries: convert the key value pair to array and return a big array
+const arr2=Object.entries(user)
+console.log(arr2);
+
+//check whether user has a key or not
+console.log(user.hasOwnProperty("name"));
+
