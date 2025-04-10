0321403d37b6139f99755fe19224edca3f6b68dc
Arman Soni <114308279+18Arman@users.noreply.github.com>
2025-04-10 10:48:42 +0000

    Learnt about higher loops

--- 03_controlflow/controlflow.js
+++ 03_controlflow/controlflow.js
@@ -69,32 +69,78 @@
 
 //for and while and do-while loop 
 //use of for loop
-for(let i=0;i<5;i++){
-    console.log(`value of i: ${i}`);
-}
+// for(let i=0;i<5;i++){
+//     console.log(`value of i: ${i}`);
+// }
 //use case of break
-for(let i=0;i<5;i++){
-    if(i==2){
-        break;
-    }
-    console.log(`value of i: ${i}`);
-}
+// for(let i=0;i<5;i++){
+//     if(i==2){
+//         break;
+//     }
+//     console.log(`value of i: ${i}`);
+// }
 //use case of continue
-for(let i=0;i<5;i++){
-    if(i==2){
-        continue;
-    }
-    console.log(`value of i: ${i}`);
-}
+// for(let i=0;i<5;i++){
+//     if(i==2){
+//         continue;
+//     }
+//     console.log(`value of i: ${i}`);
+// }
 
 //while loop: same as for loop just same changes are there
-let wh=4;
-while(wh!=0){
-    console.log(wh);
-    wh--;
-}
+// let wh=4;
+// while(wh!=0){
+//     console.log(wh);
+//     wh--;
+// }
 //do while: it will first print and then check the condition, so will be used in those cases where we need to implement at least once,
-wh=10;
-do {
-    console.log(wh);
-} while (wh<10);
+// wh=10;
+// do {
+//     console.log(wh);
+// } while (wh<10);
+
+
+
+//types of for loop
+
+//there are many types of loop like forOf, forIn, forEach and have different use cases,
+
+//forOf can be used with any Iterable Datatype, can be used with maps also
+const arr=[1,2,3,4,5];
+for (const element of arr) {
+    console.log(element);
+}
+//map, It is basically a datatype in which unique key value pairs are there and it remembers the order of insertion
+const map=new Map();
+map.set("in","india");
+map.set('usa',"united states of america");
+for (const element of map) {
+    console.log(element);// In this way both will be printed key and value both
+}
+for (const [key,value] of map) {//de-structuring the array
+    console.log(`${key} => ${value}`);
+}
+//we cannot use this with object, it is not iterable
+
+//forin
+const user={
+    name:"arman",
+    age:"20"
+}
+for (const key in user) {
+    console.log(key,user[key]);//key and key-value
+}
+//can be used with array
+
+//forEach
+const arr1=[{user:"Arman",Age:21},{user:"Jatin",Age:20}];
+arr1.forEach((item,index,arr)=> {//so with the help of forEach we can get different types of values directly like value at index, index, whole array.. 
+    //in this format we can get the values, as also mention in the code here!
+    console.log(item.user);
+    console.log(item.Age);
+});
+
+
+
+
+
